796adff635d0a481f0984380e1779f87e6282429
devilsen <375913436@qq.com>
2015-04-22 19:05:28 +0800

    加入百度地图API

--- graduation/src/main/java/com/cometous/graduation/activity/InitiateActivity.java
+++ graduation/src/main/java/com/cometous/graduation/activity/InitiateActivity.java
@@ -1,6 +1,9 @@
 package com.cometous.graduation.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.cometous.graduation.R;
 
@@ -9,9 +12,38 @@ import com.cometous.graduation.R;
  */
 public class InitiateActivity extends BaseActivity {
 
+    private mClickListener mOnclickListener;
+    private LinearLayout mLocationLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setMyContentView(R.layout.initiate_activity_layout);
+
+        mOnclickListener = new mClickListener();
+        init();
+    }
+
+    private void init() {
+        mLocationLayout = (LinearLayout) findViewById(R.id.activity_location_layout);
+        mLocationLayout.setOnClickListener(mOnclickListener);
+
+
+    }
+
+
+    class mClickListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            switch ( v.getId() ){
+                case R.id.activity_location_layout:
+                    Intent location = new Intent(InitiateActivity.this,SetLocationActivity.class);
+                    startActivity(location);
+                    break;
+            }
+
+
+        }
     }
 }
